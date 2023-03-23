6f053410c40bfd93d8938bfeab57bee495e64b1e
Bethek-Iyke <talk2bethel017@gmail.com>
2023-03-24 00:40:49 +0100

    interview question

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' code prints pascal's triangle '''
+
+def pascal_triangle(n):
+    my_list = []
+    if (n <= 0):
+        return my_list
+    my_list.append([1])
+    for i in range(n - 1):
+        my_list.append([i] + [my_list[i][a] + my_list[i][a + 1]
+            for a in range(len(my_list[i]) - 1)]  + [1])
+    return my_list
