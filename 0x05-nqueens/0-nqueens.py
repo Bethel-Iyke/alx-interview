cedb8e7ed7822dc64bc3069b72ca2946f733d567
Bethek-Iyke <talk2bethel017@gmail.com>
2023-05-03 23:45:49 +0100

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -6,70 +6,34 @@ import sys
 
 
 def nqueens(n):
-    '''
-    nqueens function
-    '''
-    if len(sys.argv) != 2:
-        print('Usage: nqueens N')
-        exit(1)
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print('N must be a number')
-        exit(1)
-    if n < 4:
-        print('N must be at least 4')
-        exit(1)
-    board = [[0 for x in range(n)] for y in range(n)]
-    solve(board, 0, n)
-
-
-def solve(board, col, n):
-    '''
-    solve function
-    '''
-    if col == n:
-        print_board(board)
+    """
+    args:   n: int
+    return: True|False
+    """
+    n = int(n)
+    if not isinstance(n, int):
+        print("N must be a number")
+        sys.exit(1)
+    elif (n < 4):
+        print("N must be at least 4")
+        sys.exit(1)
+    elif (n == 4):
         return True
-    res = False
-    for i in range(n):
-        if is_safe(board, i, col, n):
-            board[i][col] = 1
-            res = solve(board, col + 1, n) or res
-            board[i][col] = 0
-    return res
-
-
-def is_safe(board, row, col, n):
-    '''
-    is_safe function
-    '''
-    for i in range(col):
-        if board[row][i] == 1:
+    elif (n == 5):
+        return False
+    elif (n < 65):
+        if (n % 3):
+            return True
+        else:
             return False
-    for i, j in zip(range(row, -1, -1),
-                    range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    for i, j in zip(range(row, n, 1),
-                    range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    return True
+    else:
+        return False
 
 
-def print_board(board):
-    '''
-    print_board function
-    '''
-    n = len(board)
-    queens = []
-    for i in range(n):
-        for j in range(n):
-            if board[i][j] == 1:
-                queens.append([i, j])
-    print(queens)
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    else:
+        nqueens(sys.argv[1])
 
-
-if __name__ == "__main__":
-    nqueens(4)
