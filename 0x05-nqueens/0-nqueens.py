adcc5aa6112a7b19ddb6662f96f5e048fc9247c1
Bethek-Iyke <talk2bethel017@gmail.com>
2023-05-09 00:16:31 +0100

    n queen

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,79 +3,76 @@
 where the queen can be kept without colliding with other
 queen in the chess board
 """
+
+
 import sys
 
 
-def nqueens(n: int):
-    """Args: n type int
-    """
-    if not isinstance(n, int):
-        print("N must be a number \n")
-        sys.exit(1)
-    if n < 4:
-        print("N must be at least 4 \n")
-        sys.exit(1)
-
-    def backtrack(board, row, solutions):
-        """this function create another board similar to the previous board
-        """
-        if row == n:
-            solutions.append([col[:] for col in board])
-            return
-        for col in range(n):
-            if is_valid(board, row, col):
-                board[row][col] = 1
-                backtrack(board, row + 1, solutions)
-                board[row][col] = 0
-
-    board = [[0] * n for _ in range(n)]
-    solutions = []
-    backtrack(board, 0, solutions)
-    return solutions
-
-
-def is_valid(board, row, col):
-    """
-        Check this column on the current and upper rows
-        Check upper left diagonal
-    """
-    n = len(board)
-
-    for i in range(row):
-        if board[i][col] == 1:
-            return False
-
-    i = row
-    j = col
-    while i >= 0 and j >= 0:
-        if board[i][j] == 1:
-            return False
-        i -= 1
-        j -= 1
-
-    # Check upper right diagonal
-    i = row
-    j = col
-    while i >= 0 and j < n:
-        if board[i][j] == 1:
-            return False
-        i -= 1
-        j += 1
-
-    return True
-
-
-if __name__ == "__main__":
+def nqueens(n):
+    '''
+    nqueens function
+    '''
     if len(sys.argv) != 2:
-        print("Usage: python nqueens.py N")
-        sys.exit(1)
-
+        print('Usage: nqueens N')
+        exit(1)
     try:
         n = int(sys.argv[1])
     except ValueError:
-        print("N must be a number")
-        sys.exit(1)
+        print('N must be a number')
+        exit(1)
+    if n < 4:
+        print('N must be at least 4')
+        exit(1)
+    board = [[0 for x in range(n)] for y in range(n)]
+    solve(board, 0, n)
 
-    solutions = nqueens(n)
-    for solution in solutions:
-        print(solution)
+
+def solve(board, col, n):
+    '''
+    solve function
+    '''
+    if col == n:
+        print_board(board)
+        return True
+    res = False
+    for i in range(n):
+        if is_safe(board, i, col, n):
+            board[i][col] = 1
+            res = solve(board, col + 1, n) or res
+            board[i][col] = 0
+    return res
+
+
+def is_safe(board, row, col, n):
+    '''
+    is_safe function
+    '''
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1),
+                    range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, n, 1),
+                    range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
+
+
+def print_board(board):
+    '''
+    print_board function
+    '''
+    n = len(board)
+    queens = []
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] == 1:
+                queens.append([i, j])
+    print(queens)
+
+
+if __name__ == "__main__":
+    nqueens(4)
